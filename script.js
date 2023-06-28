7092302ef9680d5a1abd86c553a88a22cfc1d759
Sam <samnewell91@gmail.com>
2023-06-28 23:55:41 +0100

    Styling on Nav
    Had to change a few classes/styles since using a FA symbol on the nav bar.

--- script.js
+++ script.js
@@ -1,6 +1,6 @@
 $(".check").click(function () {
     $(".nav-top").toggleClass("nav-extend");
     $("#nav-links").toggle("400ms");
-    $(".check").toggleClass("checked");
+    $(".check i").toggleClass('checked');
 });
 
